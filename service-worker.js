d1a4357aea933bf15b2865dcf0353d4995eedfdc
Mohammad Eb <M7ammad.eb@gmail.com>
2025-03-11 00:48:35 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,35 @@
+const CACHE_NAME = 'nabled-cache-v1';
+const urlsToCache = [
+    'index.html',
+    'style.css',
+    'script.js',
+    'detail.html',
+    'detailstyle.css',
+    'detail.js',
+    // ... add any other assets you want to cache (images, fonts, etc.)
+];
 
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
