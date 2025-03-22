314f62fd62c2f5cb2e59f16828eec016ea2829a2
Mohammad Eb <M7ammad.eb@gmail.com>
2025-03-22 03:54:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -193,6 +193,7 @@ function displayItems() {
 // animation when clicking
 document.addEventListener("DOMContentLoaded", function () {
     const itemsList = document.getElementById("items-list");
+    const itemDiv = event.target.closest("div");
     
     itemDiv.querySelector('a').addEventListener('click', function(event) {
         event.preventDefault();
