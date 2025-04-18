40a04dfb6ebc5e822035e046d4b933c4959b8e15
Mohammad Eb <M7ammad.eb@gmail.com>
2025-04-18 17:25:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Increment version when static assets change
-const CACHE_NAME = 'nabled-cache-v78'; // Updated version
+const CACHE_NAME = 'nabled-cache-v79'; // Updated version
 // Adjust baseURL if deploying to a subdirectory (e.g., '/RepoName')
 const baseURL = '/NABLED-2'; // Example for GitHub Pages subdirectory
 // const baseURL = ''; // Example for root deployment
